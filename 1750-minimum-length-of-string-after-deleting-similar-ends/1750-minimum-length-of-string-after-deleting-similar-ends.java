81508654ea63ff2ea9491ca283ae4fbf2ade5716
Bora Simsek <97452363+borasimsek@users.noreply.github.com>
2024-03-06 00:16:48 +0300

    Time: 4 ms (79.81%), Space: 45.2 MB (40.99%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,23 +1,18 @@
 public class Solution {
     public int minimumLength(String s) {
         int begin = 0;
-        int end = s.length() - 1;
+        int end = s.length()-1;
         
-        while (begin < end && s.charAt(begin) == s.charAt(end)) {
+        while(begin< end && s.charAt(begin) == s.charAt(end)){
             char c = s.charAt(begin);
-
-            // onden sil
-            while (begin <= end && s.charAt(begin) == c) {
+            
+            while(begin <= end && s.charAt(begin) == c){
                 begin++;
             }
-
-            // arkadan sil
-            while (end > begin && s.charAt(end) == c) {
+            while(end>begin && s.charAt(end) == c){
                 end--;
             }
         }
-
-        // kalan kısmı ver
-        return end - begin + 1;
+        return end-begin+1;
+        }
     }
-}
